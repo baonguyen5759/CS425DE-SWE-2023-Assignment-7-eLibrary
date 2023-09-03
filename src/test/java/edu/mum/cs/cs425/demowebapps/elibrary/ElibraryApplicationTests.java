361329d456e5cd5d22b6d71fc404a072ab05cee9
baonguyen5759 <quocnb@gmail.com>
2023-09-03 11:40:00 -0500

    Assignment 7

---
+++ src/test/java/edu/mum/cs/cs425/demowebapps/elibrary/ElibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demowebapps.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
